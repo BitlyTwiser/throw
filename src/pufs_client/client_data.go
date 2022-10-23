38327b681237b6b090535433498b968d57b63be5
Josh <46653655+BitlyTwiser@users.noreply.github.com>
2022-10-23 12:30:54 -0700

    Task/show file data (#34)
    Adding a base layer for displaying file metadata in a table view and Changing time value to string and re-formatting the filedata box

--- src/pufs_client/client_data.go
+++ src/pufs_client/client_data.go
@@ -1,33 +1,40 @@
 package pufs_client
 
 import (
+	"fmt"
 	"strconv"
 
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 // Create fyne table, insert file data within.
 func FileMetadata(fileData FileData) {
 	w := fyne.CurrentApp().NewWindow("File Metadata")
-	w.Resize(fyne.NewSize(400, 400))
+	w.Resize(fyne.NewSize(400, 200))
 
 	fileSize := strconv.Itoa(int(fileData.FileSize))
 
-	data := [][]string{[]string{"FileName", "FileSize", "UpdatedAt", "IpfsHash"}, []string{
-		fileData.FileName, fileSize, fileData.UploadedAt.String(), fileData.IpfsHash,
-	}}
+	fileNameLabel := widget.NewLabel(fmt.Sprintf("File Name: %v\n--------------", fileData.FileName))
+	fileNameLabel.Wrapping = 1
 
-	table := widget.NewTable(
-		func() (int, int) {
-			return len(data), len(data[0])
-		},
-		func() fyne.CanvasObject {
-			return widget.NewLabel("File Metadata")
-		},
-		func(i widget.TableCellID, o fyne.CanvasObject) {
-			o.(*widget.Label).SetText(data[i.Row][i.Col])
-		})
+	fileSizeLabel := widget.NewLabel(fmt.Sprintf("File Size: %vMB\n------------------------", fileSize))
+	fileSizeLabel.Wrapping = 1
+
+	fileUploadedAt := widget.NewLabel(fmt.Sprintf("Uploaded at: %v\n------------", fileData.UploadedAt))
+	fileUploadedAt.Wrapping = 1
+
+	ipfsHash := widget.NewLabel(fmt.Sprintf("Ipfs Hash: %v\n--------------------", fileData.IpfsHash))
+	ipfsHash.Wrapping = 1
+
+	table := container.NewGridWithRows(
+		2,
+		fileNameLabel,
+		fileSizeLabel,
+		fileUploadedAt,
+		ipfsHash,
+	)
 
 	w.SetContent(table)
 	w.Show()
